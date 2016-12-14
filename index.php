955b1047391067d94af166b9347e81c2454adf30
erichik <erichik@gmail.com>
2016-12-15 01:23:17 +0800

    test2
    index

---
+++ index.php
@@ -0,0 +1,55 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>首頁 | 錚學院</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php include('config/js.php'); ?>
+		<?php include('config/css.php'); ?>
+
+   
+		
+
+	</head>
+	<body>
+	<?php include('template/nav.php'); ?>
+	<br><br><br>
+	<div class="jumbotron">
+		<div class="container">
+		  <h1>錚學院</h1>
+		  <p>重視觀念、循序漸進、自主學習</p>
+		  <p><a class="btn btn-primary btn-lg" href="#" role="button">瞭解更多</a></p>
+	  	</div>
+	</div>
+
+	
+	<div class="container">
+		<div class="row text-center">
+			<div class="container">
+			<div class="col-sm-4">
+				<h3>重視思考與觀念</h3>
+				<p class="text-justify">;lkj adsfl kjharas rg sfg swr4gs rgsarg srg w345rg seroiuh akjdsfn iaurhf aksjdhbfoi augw4eirf uaglkjefgha ouewhf baoj erfa bfe</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>循序漸進</h3>
+				<p class="text-justify">g srg w345rg seroiuh akjdsfn iaurhf aksjdhbfoi augw4eirf uaglkje;lkj adsfl kjharas rg sfg swr4gs rgsarfgha ouewhf baoj erfa bfe</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>自主學習</h3>
+				<p class="text-justify">g w345rg seroiuh akjdsfn iaurhf aksjdhbfoi au;lkj adsfl kjharas rg sfg swr4gs rgsarg srgw4eirf uaglkjefgha ouewhf baoj erfa bfe</p>
+			</div>
+			</div>
+		</div>
+	</div>
+	
+		
+		
+		
+		
+	<?php include('template/footer.php'); ?>
+	</body>
+</html>
